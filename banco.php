5a8569138ba056ede2ca60c20d9c796aa1de4737
Aline Alves Borges <llyne_slo@hotmail.com>
2026-06-27 16:12:40 -0300

    Corrigindo banco.php para usar variáveis de ambiente do Railway

--- banco.php
+++ banco.php
@@ -1,11 +1,13 @@
 <?php
-$host = "localhost";
-$dbname = "projeto_volunteer_community";
-$user = "postgres";
-$pass = "1234";
+// Pegando as variáveis do Railway
+$host = getenv('PGHOST') ?: 'localhost';
+$port = getenv('PGPORT') ?: '5432';
+$dbname = getenv('PGDATABASE') ?: 'projeto_volunteer_community';
+$user = getenv('PGUSER') ?: 'postgres';
+$password = getenv('PGPASSWORD') ?: '1234';
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass, [
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false
